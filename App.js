e6918913f226cbe38ef5bd37b33566a6f5a22a0f
185493676 <185493676@qq.com>
2018-05-25 13:26:25 +0800

    去掉页面底部黄色警告

--- App.js
+++ App.js
@@ -4,28 +4,27 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    StyleSheet,
+    YellowBox,
 } from 'react-native';
 import RouterConfig from './app/RouterConfig';
 
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 export default class App extends Component<Props> {
-  render() {
-    return (
-        <RouterConfig/>
-    );
-  }
+    render() {
+        return (
+            <RouterConfig/>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
 });
