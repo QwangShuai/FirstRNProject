09fe150e314e9f7ea4edd0573eb425c3f2b1d802
weibo <weibo@shpso.com>
2018-05-26 10:12:45 +0800

    bugFix

--- app/components/PersonalInfoItem.js
+++ app/components/PersonalInfoItem.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
-import {AppRegistry,View, StyleSheet,ImageBackground} from 'react-native';
+import {View, StyleSheet,Image} from 'react-native';
+import UtilScreen from '../util/UtilScreen';
 export default class PersonalInfoItem extends Component{
     constructor(props){
         super(props);
     }
+    static defaultProps = {
+        itemInfo:{
+            imageURL:"require('../res/images/user-1.png')",
+            lTitle:'左边主标题',
+            rValue:'右边的值',
+        },
+        imageURL:"require('../res/images/user-1.png')",
+
+
+    }
     render(){
-        return ;
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={this.props.imageURL}
+                    resizeMode='stretch'
+                />
+            </View>
+        );
     }
 }
+const styles=StyleSheet.create({
+    container:{
+        width:'100%',
+        height:UtilScreen.getHeight(86),
+        backgroundColor:'red',
+    }
+});
--- app/router/RouterConfig.js
+++ app/router/RouterConfig.js
@@ -5,12 +5,15 @@ import WaitingLeaf from '../components/WaitingLeaf';
 import Registered from '../pages/Registered';
 import Set from '../pages/Set';
 import PersonalInfo from '../pages/PersonalInfo';
+import PersonalInfoItem from '../components/PersonalInfoItem';
 const RouterConfig = StackNavigator({
     Home: { screen: LoginLeaf },
     Wait: { screen: WaitingLeaf },
     Register:{screen:Registered},
     Set:{screen:Set},
     PersonalInfo:{screen:PersonalInfo},
+    PersonalInfoItem:{screen:PersonalInfoItem},
+
 
 });
 export default RouterConfig;
