9ecabffe7ce97c66ad4f6d061e4746f7c42702fa
185493676 <185493676@qq.com>
2018-06-01 10:07:51 +0800

    bugFix

--- app/router/RouterConfig.js
+++ app/router/RouterConfig.js
@@ -30,6 +30,7 @@ const RouterConfig = StackNavigator({
     // ApplyPaymentSuccess:{screen:ApplyPaymentSuccess},
     // ApplyRealName:{screen:ApplyRealName},
     // ApplyPaymentState:{screen:ApplyPaymentState},
+    
     Apply:{screen:Apply},
     Home: {screen: LoginLeaf},
     Wait: {screen: WaitingLeaf},
