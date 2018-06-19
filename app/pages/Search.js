c006304a630d5f9e138d6a4e9553a24332f5016e
Qwangshuai <634134493@qq.com>
2018-06-19 09:17:03 +0800

    添加搜索页

--- app/pages/Search.js
+++ app/pages/Search.js
@@ -12,34 +12,58 @@ export default class Search extends Component {
         super(props);
     }
 
+    backClick(){
+
+    }
+    search(){
+
+    }
     render(){
         return(
             <View style={Stylecss.styles.container}>
-                <View style={styles.searchStyle}>
-                    <Image style={styles.imageStyle} source={require('../res/images/6F.png')} resizeMode='contain'/>
-                    <TextInput style={{flex:1,fontSize:14,padding:0}} placeholder='搜索文章/景点' underlineColorAndroid='transparent'/>
+                <View style={styles.myView}>
+                    <View style={styles.searchStyle}>
+                        <Image style={styles.imageStyle} source={require('../res/images/6F.png')} resizeMode='contain'/>
+                        <TextInput style={{flex:1,fontSize:14,padding:0}} placeholder='搜索文章/景点' underlineColorAndroid='transparent'
+                                   onChangeText={this.search.bind(this)}/>
+                    </View>
+                    <Text style={styles.cancelStyle} onPress={this.backClick.bind(this)}>取消</Text>
                 </View>
+
             </View>
         )
     }
 }
 const styles = StyleSheet.create({
-    searchStyle:{
+    myView:{
+        marginTop:UtilScreen.getHeight(20),
+        width:'100%',
+        height:UtilScreen.getHeight(64),
+        alignSelf:'center',
+        backgroundColor:'#ffffff',
         flexDirection:'row',
-        margin:UtilScreen.getWidth(20),
-        fontSize:14,
+    },
+    searchStyle:{
+        marginLeft:UtilScreen.getWidth(20),
+        flexDirection:'row',
         borderRadius:UtilScreen.getWidth(34),
         width:UtilScreen.getWidth(634),
         height:UtilScreen.getHeight(64),
         borderWidth:UtilScreen.getWidth(2),
         borderColor:'#FEA81F',
-        alignSelf:'center',
-        justifyContent:'center',
     },
     imageStyle:{
         margin:UtilScreen.getWidth(24),
         alignSelf:'center',
         width:UtilScreen.getWidth(24),
         height:UtilScreen.getHeight(24),
+    },
+    cancelStyle:{
+        marginLeft:UtilScreen.getHeight(20),
+        lineHeight:UtilScreen.getHeight(64),
+        textAlign:'center',
+        fontSize:14,
+        color:'#333333',
+        alignSelf:'center',
     },
 })
