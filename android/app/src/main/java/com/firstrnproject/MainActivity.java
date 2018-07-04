403aa42f1591bf564d0b3fd7d4b75ef9eec37417
QwangShuai <31304278+QwangShuai@users.noreply.github.com>
2018-07-04 18:11:10 +0800

    集成云信依赖

--- android/app/src/main/java/com/firstrnproject/MainActivity.java
+++ android/app/src/main/java/com/firstrnproject/MainActivity.java
@@ -1,6 +1,12 @@
 package com.firstrnproject;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+
 import com.facebook.react.ReactActivity;
+import com.netease.im.RNNeteaseImModule;
+import com.netease.im.ReceiverMsgParser;
+import com.netease.im.uikit.permission.MPermission;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -14,5 +20,18 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "FirstRNProject";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(ReceiverMsgParser.checkOpen(getIntent())){//在后台时处理点击推送消息
+            RNNeteaseImModule.launch = getIntent();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        MPermission.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
     }
 }
