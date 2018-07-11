b8ae8577ac9c80d561aa8c2b347c70b58df7ba3b
QwangShuai <31304278+QwangShuai@users.noreply.github.com>
2018-07-11 10:09:42 +0800

    添加视频页

---
+++ app/pages/FriendsTogetherVideo.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import UtilScreen from '../util/UtilScreen';
+import ToolBar from '../components/ToolBar';
+import {Video} from 'react-native-video';
